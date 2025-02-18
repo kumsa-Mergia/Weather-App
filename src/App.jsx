1dee2078c1a82223bc2067e783ae137d9dcd0c61
kumsa Mergia <kumsamergia@gmail.com>
2025-02-18 12:42:53 +0300

    App jsx modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import axios from "axios";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  // Initialize the state for 'data' correctly
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+  const url = `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=a89c714d994a9a01f13358c9f927984d`;
+  
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      })
+      setLocation('')
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="search">
+        <input
+          value={location}
+          onChange={event => setLocation(event.target.value)}
+          onKeyUp={searchLocation}
+          placeholder="Enter Location" 
+          type="text" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="container">
+        {/* Top Section */}
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p> {/* Display the location name */}
+          </div>
+          <div className="temp">
+          {data.main ? <h1>{data.main.temp}</h1> : null } {/* Display the temperature */}
+          </div>
+          <div className="description">
+            <p>{data.weather ? data.weather[0].description : null}</p> {/* Weather description */}
+          </div>
+        </div>
+        {/* Bottom Section */}
+        <div className="bottom">
+          <div className="feels">
+            <p className="bold">{data.main ? `${data.main.feels_like} °F` : null}</p>
+          </div>
+          <div className="humidity">
+            <p className="bold">{data.main ? `${data.main.humidity}%` : null}</p>
+          </div>
+          <div className="wind">
+            <p className="bold">{data.wind ? `${data.wind.speed} MPH` : null}</p>
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
